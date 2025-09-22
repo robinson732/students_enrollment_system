b8a4d2d7ce3389875de7ff03f3fdc79d0dd1de44
wilson Merengo <merengowilson2@gmail.com>
2025-09-22 11:51:00 +0300

    Wilson-frontend

--- client/students-enrollment-course/src/App.jsx
+++ client/students-enrollment-course/src/App.jsx
@@ -1,1 +1,28 @@
- 
+import { useState, useEffect } from "react";
+import StudentList from "./components/StudentList";
+import CourseList from "./components/CourseList";
+import EnrollmentList from "./components/EnrollmentList";
+
+function App() {
+  const [students, setStudents] = useState([]);
+  const [courses, setCourses] = useState([]);
+  const [enrollments, setEnrollments] = useState([]);
+
+   
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/students").then(r => r.json()).then(setStudents);
+    fetch("http://127.0.0.1:5000/courses").then(r => r.json()).then(setCourses);
+    fetch("http://127.0.0.1:5000/enrollments").then(r => r.json()).then(setEnrollments);
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>📚 Student Course Enrollment</h1>
+      <StudentList students={students} setStudents={setStudents} />
+      <CourseList courses={courses} setCourses={setCourses} />
+      <EnrollmentList enrollments={enrollments} students={students} courses={courses} setEnrollments={setEnrollments} />
+    </div>
+  );
+}
+
+export default App;
